13fdc5b9cd976dc625803d974ec1397462d4944b
Ilja Gusiatin <igoose@macbook-pro>
2025-08-13 13:20:45 +0300

    feat: retry and error handling improvements

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
-import { QueryClient, QueryClientProvider, QueryCache } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useErrorStore } from './stores/errorStore';
 import { isAxiosError, type AxiosError } from 'axios';
 
@@ -12,20 +12,25 @@ const queryClient = new QueryClient({
     queries: {
       retry: (failureCount, error: Error | AxiosError) => {
         if (isAxiosError(error)) {
-          if (error.status && error.status >= 401) {
+          const errorStatus = error.status || 500;
+          // Dont retry on client errors
+          if (errorStatus >= 400 && errorStatus < 500) {
+            useErrorStore.getState().handleError(error);
             return false;
+          } else {
+            if (failureCount === 3) {
+              useErrorStore.getState().handleError(error); // Handle errors on last retry
+            }
+            return failureCount < 3;
           }
         }
-        // Default retry logic (3 times)
+        if (failureCount === 3) {
+          useErrorStore.getState().handleError(error);
+        }
         return failureCount < 3;
       },
     },
   },
-  queryCache: new QueryCache({
-    onError: (error) => {
-      useErrorStore.getState().handleError(error);
-    },
-  }),
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
